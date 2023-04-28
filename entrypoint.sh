12aa13f5275ebdee5cd7689cd8d5c2143ba497ba
Alexander Berg <nyquist.alexander@gmail.com>
2023-04-28 17:56:52 +0200

    Removed debug prints from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,10 +39,7 @@ fi
 if ! flyctl status --app "$app"; then
   flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
   if [ -n "$INPUT_SECRETS" ]; then
-    echo "GOT SECRETS $INPUT_SECRETS"
     echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
-  else
-    echo "GOT NO SECRETS"
   fi
   flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
 elif [ "$INPUT_UPDATE" != "false" ]; then
